87a47d676c28bf0e143ee3dc9519d75ec1744f94
sirojsms1712@gmail.com <sirojsms1712@gmail.com>
2023-12-29 16:37:00 +0500

    after testing build

--- src/main/java/com/example/library/LibApplication.java
+++ src/main/java/com/example/library/LibApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppNewsSiteApplication {
+public class LibApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AppNewsSiteApplication.class, args);
+        SpringApplication.run(LibApplication.class, args);
     }
 }
--- src/test/java/com/example/library/LibApplicationTests.java
+++ src/test/java/com/example/library/LibApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.example.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppNewsSiteApplicationTests {
+class LibApplicationTests {
 
     @Test
     void contextLoads() {
